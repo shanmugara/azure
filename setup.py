a306dfe5b704e4037c7aedb8043ffa380010b721
shanmugara <psraj@optonline.net>
2021-07-02 15:52:15 -0400

    add setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(name='azuregraph',
       author='MS',
       author_email='',
       license='GPL',
-      install_requires= ['requests',
+      install_requires=['requests',
                         ],
+      packages=['qc', 'qc.common', 'qc.helpers', 'qc.appmodules', 'qc.appmodules.apache'],
       entry_points={
-                    'console_scripts':
-                        ['callgraph = runner:main'],
-                    },
+          'console_scripts':
+              ['callgraph = .runner:main'],
+      },
       include_package_data=True
-)
+      )
