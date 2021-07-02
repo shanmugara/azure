8da6216e71ca3107ac00e4db18a658aaefc6c7b3
shanmugara <psraj@optonline.net>
2021-07-02 19:22:05 -0400

    add setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ setup(name='azuregraph',
       version="0.0.1",
       description='Azure Graph API wrapper.',
       url='https://none.none',
-      author='MS',
-      author_email='',
+      author='RP',
+      author_email='speriyasamy@',
       license='GPL',
-      install_requires=['requests',
-                        'msal',
-                        ],
-      packages=['az'],
+      install_requires=[
+          'requests',
+          'msal',
+      ],
+      package_dir={"": "src"},
       entry_points={
           'console_scripts':
-              ['callgraph = az.runner:main'],
+              ['callgraph = src.runner:main'],
       },
-      include_package_data=True
+      include_package_data=True,
+      zip_safe=False,
       )
