b8b77be7817da40b399dd8808054bd48132e682a
shanmugara <psraj@optonline.net>
2021-07-16 12:34:45 -0400

    more clean up

--- src/az/helpers/powershell.py
+++ src/az/helpers/powershell.py
@@ -1,8 +1,38 @@
 from subprocess import check_output
 import os
+import functools
+import timeit
+import platform
 
 app_root = os.path.split(os.path.abspath(__file__))[0]
 
+from az.helpers import my_logger
+
+if platform.system().lower() == 'windows':
+    LOG_DIR = os.path.join('c:\\', 'logs', 'azgraph')
+else:
+    LOG_DIR = os.path.join(os.environ['VIRTUAL_ENV'], 'logs', 'azgraph')
+
+log = my_logger.My_logger(logdir=LOG_DIR, logfile='azuread')
+
+def add_timer(func):
+    functools.wraps(func)
+
+    def timed_func(*args, **kwargs):  # Inner func return func
+        start_time = timeit.default_timer()
+        func_results = func(*args, **kwargs)
+        end_time = timeit.default_timer()
+        elapsed_time = end_time - start_time
+        log.info(
+            "Function {} - Elapsed time: {}".format(
+                func.__name__, round(elapsed_time, 3)
+            )
+        )
+        return func_results
+
+    return timed_func
+
+@add_timer
 def get_adgroupmember(groupname):
     """
     get ad group members
