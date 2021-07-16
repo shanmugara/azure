bd564d15cb154796802a3bb353ad0f7d545a251f
shanmugara <psraj@optonline.net>
2021-07-16 12:37:05 -0400

    more clean up

--- src/az/helpers/powershell.py
+++ src/az/helpers/powershell.py
@@ -13,7 +13,7 @@ if platform.system().lower() == 'windows':
 else:
     LOG_DIR = os.path.join(os.environ['VIRTUAL_ENV'], 'logs', 'azgraph')
 
-log = my_logger.My_logger(logdir=LOG_DIR, logfile='azuread')
+log = my_logger.My_logger(logdir=LOG_DIR, logfile=__name__)
 
 def add_timer(func):
     functools.wraps(func)
