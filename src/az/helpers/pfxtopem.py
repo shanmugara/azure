2831a58b9f1a5d4395e9c057fe59019271843154
shanmugara <psraj@optonline.net>
2021-07-18 19:35:26 -0400

    add pfx exract

--- src/az/helpers/pfxtopem.py
+++ src/az/helpers/pfxtopem.py
@@ -3,6 +3,7 @@ A module to extract cert and key from a PFX file.
 """
 from contextlib import contextmanager
 from pathlib import Path
+import os
 from tempfile import NamedTemporaryFile
 
 import requests
@@ -12,23 +13,29 @@ from cryptography.hazmat.primitives.serialization.pkcs12 import load_key_and_cer
 
 @contextmanager
 def pfx_to_pem(pfx_path, pfx_password):
-    ''' Decrypts the .pfx file to be used with requests. '''
-    print('step 1')
+    '''
+    Decrypts the .pfx file to cert and key file
+    :param pfx_path: pfx file path to extract
+    :param pfx_password: password for pfx file
+    '''
+
     pfx = Path(pfx_path).read_bytes()
-    print('step 2')
     private_key, main_cert, add_certs = load_key_and_certificates(pfx, pfx_password.encode('utf-8'), None)
 
-    cert_file_out = "mycertfile.pem"
-    cert_key_out = "mycertkey.pem"
+    out_dir = os.path.split(Path(pfx_path))[0]
+    cert_file_out = os.path.join(out_dir,"mycertfile.pem")
+    cert_key_out = os.path.join(out_dir, "mycertkey.pem")
 
     with open(cert_key_out, 'wb') as pem_file:
-        print('step 3')
+        print("*** Writing key file to {}".format(cert_key_out))
         pem_file.write(private_key.private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption()))
-        for ca in add_certs:
-            pem_file.write(ca.public_bytes(Encoding.PEM))
+
 
     with open(cert_file_out, 'wb') as pem_file:
+        print("*** Writing cert file to {}".format(cert_file_out))
         pem_file.write(main_cert.public_bytes(Encoding.PEM))
+        for ca in add_certs:
+            pem_file.write(ca.public_bytes(Encoding.PEM))
 
 
 # HOW TO USE:
