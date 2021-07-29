9d72d2279be37ab564a9e667cc1c92d2d784297f
shanmugara <psraj@optonline.net>
2021-07-28 21:34:54 -0400

    add README.md

--- src/az/rest/azureauth.py
+++ src/az/rest/azureauth.py
@@ -191,7 +191,7 @@ class AzureAd(object):
         elif loginid:
             query_str = "?$filter=startswith(userPrincipalname, '{}@')".format(loginid)
         else:
-            filter = "?$top=600&$select=onPremisesSyncEnabled,id,userPrincipalName,businessPhones,displayName,givenName," \
+            filter = "?$top=999&$select=onPremisesSyncEnabled,id,userPrincipalName,businessPhones,displayName,givenName," \
                      "jobTitle,mail,mobilePhone,officeLocation,surname"
             query_str = filter
         page = True
